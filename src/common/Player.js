78230320b30b585b46892b7d2e4629bcddccbf98
Gary Weiss <garyweiss1@yahoo.com>
2016-11-22 22:55:08 +0200

    simpler vanilla game object

--- src/common/Player.js
+++ src/common/Player.js
@@ -8,13 +8,6 @@ class Player extends DynamicObject {
         return Object.assign({}, super.netScheme);
     }
 
-    static newFrom(sourceObj) {
-        let newPlayer = new Player();
-        newPlayer.copyFrom(sourceObj);
-
-        return newPlayer;
-    }
-
     constructor(id, x, y) {
         super(id, x, y);
         this.class = Player;
