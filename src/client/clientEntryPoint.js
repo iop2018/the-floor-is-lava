dca01217fe2bcecd89890c130c5e9baafe672a2a
Gary Weiss <fourvelocity@gmail.com>
2017-10-19 10:37:02 +0300

    add new files

---
+++ src/client/clientEntryPoint.js
@@ -0,0 +1,25 @@
+import querystring from 'query-string';
+import MyClientEngine from '../client/MyClientEngine';
+import MyGameEngine from '../common/MyGameEngine';
+const qsOptions = querystring.parse(location.search);
+
+// default options, overwritten by query-string options
+// is sent to both game engine and client engine
+const defaults = {
+    traceLevel: 1,
+    delayInputCount: 3,
+    clientIDSpace: 1000000,
+    syncOptions: {
+        sync: qsOptions.sync || 'extrapolate',
+        localObjBending: 0.0,
+        remoteObjBending: 0.8,
+        bendingIncrements: 6
+    }
+};
+let options = Object.assign(defaults, qsOptions);
+
+// create a client engine and a game engine
+const gameEngine = new MyGameEngine(options);
+const clientEngine = new MyClientEngine(gameEngine, options);
+
+document.addEventListener('DOMContentLoaded', function(e) { clientEngine.start(); });
