d45411ca26b835afd9b1c83f613a1e1de8418918
Gary Weiss <fourvelocity@gmail.com>
2017-10-19 12:22:00 +0300

    use KeyboardControls

--- src/client/MyClientEngine.js
+++ src/client/MyClientEngine.js
@@ -1,64 +1,18 @@
 import ClientEngine from 'lance/ClientEngine';
 import MyRenderer from '../client/MyRenderer';
+import KeyboardControls from 'lance/controls/KeyboardControls';
 
 export default class MyClientEngine extends ClientEngine {
 
     constructor(gameEngine, options) {
         super(gameEngine, options, MyRenderer);
 
-        this.gameEngine.on('client__preStep', this.preStep.bind(this));
-
-        // keep a reference for key press state
-        this.pressedKeys = {
-            down: false,
-            up: false,
-            left: false,
-            right: false,
-            space: false
-        };
-
-        let that = this;
-        document.onkeydown = (e) => { that.onKeyChange(e, true); };
-        document.onkeyup = (e) => { that.onKeyChange(e, false); };
+        this.controls = new KeyboardControls(this);
+        this.controls.bindKey('up', 'up', { repeat: true } );
+        this.controls.bindKey('down', 'down', { repeat: true } );
+        this.controls.bindKey('left', 'left', { repeat: true });
+        this.controls.bindKey('right', 'right', { repeat: true });
+        this.controls.bindKey('space', 'space');
     }
 
-    // our pre-step is to process all inputs
-    preStep() {
-
-        if (this.pressedKeys.up) {
-            this.sendInput('up', { movement: true });
-        }
-
-        if (this.pressedKeys.down) {
-            this.sendInput('down', { movement: true });
-        }
-
-        if (this.pressedKeys.left) {
-            this.sendInput('left', { movement: true });
-        }
-
-        if (this.pressedKeys.right) {
-            this.sendInput('right', { movement: true });
-        }
-
-        if (this.pressedKeys.space) {
-            this.sendInput('space', { movement: true });
-        }
-    }
-
-    onKeyChange(e, isDown) {
-        e = e || window.event;
-
-        if (e.keyCode == '38') {
-            this.pressedKeys.up = isDown;
-        } else if (e.keyCode == '40') {
-            this.pressedKeys.down = isDown;
-        } else if (e.keyCode == '37') {
-            this.pressedKeys.left = isDown;
-        } else if (e.keyCode == '39') {
-            this.pressedKeys.right = isDown;
-        } else if (e.keyCode == '32') {
-            this.pressedKeys.space = isDown;
-        }
-    }
 }
